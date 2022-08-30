41641efe540047e67151a8e095bbf953a6ecbe0c
jeniththakkar007 <jenith.thakkar008@gmail.com>
2022-08-30 12:23:18 +0530

    Connection String Changes

--- ShoppiteAPI/Shoppite.Infrastructure/Data/GeneralDbContext.cs
+++ ShoppiteAPI/Shoppite.Infrastructure/Data/GeneralDbContext.cs
@@ -8,6 +8,6 @@ namespace Shoppite.Core.Entities
 {
     public class GeneralDbContext
     {
-        public string ConnectionString = "Data Source=DESKTOP-P9GI997;Initial Catalog=Shoppite;Integrated Security=True";
+        public string ConnectionString = "Data Source=103.150.186.216;Initial Catalog=Shoppite;User id=sa;Password=Z8Lix[jg3K@R74";
     }
 }
