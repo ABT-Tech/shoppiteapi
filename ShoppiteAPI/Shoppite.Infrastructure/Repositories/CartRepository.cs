abe2e4ce83c3b0fc767785afffad528edd2b8c89
Vrusti001 <Vrusti.manavadariya@ashokinfotech.in>
2023-04-19 16:29:50 +0530

    commit mycart page

--- ShoppiteAPI/Shoppite.Infrastructure/Repositories/CartRepository.cs
+++ ShoppiteAPI/Shoppite.Infrastructure/Repositories/CartRepository.cs
@@ -91,6 +91,7 @@ namespace Shoppite.Infrastructure.Repositories
                         cartDTO.Image = result["Image"].ToString();
                         cartDTO.Brand = result["Brand"].ToString();
                         cartDTO.Quantity = Convert.ToInt32(result["Quantity"]);
+                        cartDTO.ProductQty = Convert.ToInt32(result["qty"]);
                         cartDTO.Price = Convert.ToDouble(result["Price"]);
                         cartDTO.OldPrice = result["OldPrice"] != DBNull.Value ? Convert.ToInt32(result["OldPrice"]):0;
                         cartDTO.orgId = Convert.ToInt32(OrgId);
