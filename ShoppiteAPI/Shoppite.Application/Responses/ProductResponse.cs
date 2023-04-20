32b51b712a8d44c265c82e699474dcbdca18ba09
Jayshree Solanki <jayshree.solanki@ashokinfotech.in>
2023-04-20 14:54:47 +0530

    wishlist changes

--- ShoppiteAPI/Shoppite.Application/Responses/ProductResponse.cs
+++ ShoppiteAPI/Shoppite.Application/Responses/ProductResponse.cs
@@ -18,6 +18,6 @@ namespace Shoppite.Application.Responses
         public int orgId { get; set; }
         public string[] ProductList { get; set; }
         public int OldPrice { get; set; }
-        public int? WishlistedProduct { get; set; }
+        public Boolean WishlistedProduct { get; set; }
     }
 }
