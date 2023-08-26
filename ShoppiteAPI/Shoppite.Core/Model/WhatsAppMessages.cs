2564cdf3f8712adb9186d0765fb1efdc602b3d51
jeniththakkar007 <jenith.thakkar008@gmail.com>
2023-08-26 16:28:00 +0530

    Send WhatsappMessage in API call

---
+++ ShoppiteAPI/Shoppite.Core/Model/WhatsAppMessages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppite.Core.Model
+{
+    public partial class WhatsAppMessages
+    {
+        public int ID { get; set; }
+        public string MobileNumber { get; set; }
+        public string MessageRequest { get; set; }
+        public string TemplateID { get; set; }
+        public bool Is_SendMessage { get; set; }
+        public string MessageResponse { get; set; }
+        public string OrgName { get; set; }
+        public DateTime? InsertDateTime { get; set; }
+    }
+}
